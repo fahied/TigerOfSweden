0a78b0c8dc8fd0fe362e94f215a0842abeba880b
Fahied <fahied@gmail.com>
2014-06-11 18:19:22 +0200

    added app constant class

---
+++ TigerOfSweden/AppConstants.h
@@ -0,0 +1,15 @@
+//
+//  AppConstants.h
+//  TigerOfSweden
+//
+//  Created by Muhammad Fahied on 11/06/14.
+//  Copyright (c) 2014 Muhammad Fahied. All rights reserved.
+//
+
+#ifndef TigerOfSweden_AppConstants_h
+#define TigerOfSweden_AppConstants_h
+
+
+#define SERVER_URL [NSURL URLWithString:@"https://server.com"]
+
+#endif
