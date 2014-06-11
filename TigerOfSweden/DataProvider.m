818d381c72d7e7cb31d780fe33edde2d0e18b556
Fahied <fahied@gmail.com>
2014-06-11 18:19:57 +0200

    data provider class that provide data to uitablview
    Main Features: fetch and parse data

---
+++ TigerOfSweden/DataProvider.m
@@ -0,0 +1,13 @@
+//
+//  DataProvider.m
+//  TigerOfSweden
+//
+//  Created by Muhammad Fahied on 11/06/14.
+//  Copyright (c) 2014 Muhammad Fahied. All rights reserved.
+//
+
+#import "DataProvider.h"
+
+@implementation DataProvider
+
+@end
